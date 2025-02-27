9b124124078e79e5b5bb6a493d0902be486ba92a
Dave jal <newagesocietyaltsoc@gmail.com>
2025-02-27 16:37:42 +0000

    c4

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ require('dotenv').config();
 const app = express();
  
 // Middleware
-app.use(cors()); 
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: '*',
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
